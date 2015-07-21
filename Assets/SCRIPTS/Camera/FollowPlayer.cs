90f8b3538137c135532f76b0436801212df72b16
Paul Maurer <maurer.paul@ymail.com>
2015-07-21 18:20:12 +0200

    3C + Incontrol

---
+++ Assets/SCRIPTS/Camera/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	
+	public GameObject player;
+	public int distance;
+	public Vector3 velocity = Vector3.zero;
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = Vector3.SmoothDamp(transform.position, player.transform.position, ref velocity, 0.10f);
+		transform.position = new Vector3 (transform.position.x, transform.position.y, distance);
+	}
+}
---
+++ Assets/SCRIPTS/Player/Controller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class Controller : MonoBehaviour {
+
+	Rigidbody rb;
+	float x;
+	float y;
+	public int force;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var inputDevice = InputManager.ActiveDevice;
+		x = -inputDevice.LeftStickX;
+		y = inputDevice.LeftStickY;
+
+		if ((x != 0.0f || y != 0.0f) && (x > 0.3f || x < -0.3f || y > 0.3f || y < -0.3f)) {
+			float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(90.0f - angle, Vector3.forward);
+		}
+
+		if (inputDevice.Action1.WasPressed) {
+			rb.AddForce(transform.up * -force);
+		}
+	}
+}
