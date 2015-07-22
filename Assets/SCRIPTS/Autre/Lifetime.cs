0ee1405173efec13048ae1ad36130381f68fd2f2
Paul Maurer <maurer.paul@ymail.com>
2015-07-23 00:04:00 +0200

    ajout d'eye candy

---
+++ Assets/SCRIPTS/Autre/Lifetime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lifetime : MonoBehaviour {
+
+	public float lifetime;
+	float timer;
+
+	void Start () {
+		timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		timer += 0.1f;
+		if (timer >= lifetime)
+			Destroy (gameObject);
+	}
+}
--- Assets/SCRIPTS/PowerUps/PowerUp.cs
+++ Assets/SCRIPTS/PowerUps/PowerUp.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class PowerUp : MonoBehaviour {
 
 	public GameObject pickUpBullet;
-
+	public GameObject effectWhenCollected;
 	public void DestroySelf() {
 		Destroy (this.gameObject);
 	}
 	public void Used() {
+		Instantiate (effectWhenCollected, transform.position, Quaternion.identity);
 		DestroySelf ();
 	}
 
