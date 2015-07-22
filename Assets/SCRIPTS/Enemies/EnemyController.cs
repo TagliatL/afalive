79fc033f94a20f19673373a5ee9803475a2a45cd
noisegrrrl <emualynk@gmail.com>
2015-07-23 00:04:14 +0200

    TECHNO TOUJOURS PAREIL BOOM BOOM DANS LES OREILLES

--- Assets/SCRIPTS/Enemies/EnemyController.cs
+++ Assets/SCRIPTS/Enemies/EnemyController.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
-	public Transform target;
+ 	private Transform target;
 	public float speed;
-	//public float rotationSpeed;
+	public float rotationSpeed;
 
 	void Start() {
 		target = null;
@@ -14,8 +14,7 @@ public class EnemyController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (target != null) {
-			Quaternion rotation = Quaternion.LookRotation
-				(target.transform.position - transform.position, transform.TransformDirection(-Vector3.forward));
+			Quaternion rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position, transform.TransformDirection(-Vector3.forward)), rotationSpeed * Time.deltaTime);
 			transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
 			transform.position = transform.position + (transform.up * speed * Time.deltaTime);
 		}
