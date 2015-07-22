8eccf180bc254dca853e1d913e1c2f5f8fb528cd
noisegrrrl <emualynk@gmail.com>
2015-07-22 23:53:49 +0200

    TEUF TEUF TEUF FAIT LE TRAIN TRAIN TRAIN 2

--- Assets/SCRIPTS/Enemies/EnemyController.cs
+++ Assets/SCRIPTS/Enemies/EnemyController.cs
@@ -14,9 +14,6 @@ public class EnemyController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (target != null) {
-			Quaternion rotation = Quaternion.LookRotation
-				(target.transform.position - transform.position, transform.TransformDirection(-Vector3.forward));
-			transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
 			Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position), rotationSpeed * Time.deltaTime);
 			transform.position = transform.position + (transform.up * speed * Time.deltaTime);
 		}
