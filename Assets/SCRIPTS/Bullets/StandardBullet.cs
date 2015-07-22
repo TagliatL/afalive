7da0e199087b07ebb58076d7883d2978e0771bc6
noisegrrrl <emualynk@gmail.com>
2015-07-22 22:53:25 +0200

    bullet cleanup
    TEUF TEUF TEUF FAIT LE TRAIN TRAIN TRAIN

--- Assets/SCRIPTS/Bullets/StandardBullet.cs
+++ Assets/SCRIPTS/Bullets/StandardBullet.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class StandardBullet : BulletManager {
 
+	void OnEnable() {
+		Object.Destroy(this.gameObject, this.lifeTime);
+	}
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Vector3.up * Time.deltaTime * speed *10);
