4a387e2b6853bdc38087ebc1622d62e62d963fa2
noisegrrrl <emualynk@gmail.com>
2015-07-22 18:21:33 +0200

    Fixed fire rate
    CA MARCHE COMME CA DEVRAIT POUR TIRER

--- Assets/SCRIPTS/Player/Controller.cs
+++ Assets/SCRIPTS/Player/Controller.cs
@@ -7,52 +7,32 @@ public class Controller : MonoBehaviour {
 	Rigidbody rb;
 	float x;
 	float y;
-	bool isContinue;
+	float counter;
 	public GameObject equippedBullet;
 	GameObject instanciatedBullet;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
-		isContinue = false;
+		counter = 0f;
 	}
 
-	void ResetFire() {
-		isContinue = false;
-	}
-
-	//coroutine de tir
-	IEnumerator ContinuedFire(BulletManager bulletValues) {
-		var inputDevice = InputManager.ActiveDevice;
-		while (true) {
-			if(inputDevice.Action1.IsPressed) {
-				rb.AddForce(transform.up * -bulletValues.force);
-				instanciatedBullet = Instantiate (equippedBullet, transform.position, transform.rotation) as GameObject;
-			}
-			//ceci permet en théorie de faire ce que je veux mais quand tu testes tu vois que des fois y'a trop de bullets qui partent
-			Invoke("ResetFire",bulletValues.rateOfFire);
-			yield return new WaitForSeconds(bulletValues.rateOfFire);
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	void FixedUpdate() {
 		BulletManager bulletValues = equippedBullet.GetComponent<BulletManager> ();
 		rb.angularVelocity = Vector3.zero;
 		var inputDevice = InputManager.ActiveDevice;
 		x = -inputDevice.LeftStickX;
 		y = inputDevice.LeftStickY;
-
+		
 		if ((x != 0.0f || y != 0.0f) && (x > 0.3f || x < -0.3f || y > 0.3f || y < -0.3f)) {
 			float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
 			transform.rotation = Quaternion.AngleAxis(90.0f - angle, Vector3.forward);
 		}
-		//lance la coroutine si elle n'est pas déjà lancé
-		if (inputDevice.Action1.WasPressed && !isContinue) {
-			StartCoroutine(ContinuedFire(bulletValues));
-			isContinue = true;
+
+		if (inputDevice.Action1.IsPressed && (counter <= 0f)) {
+			counter = bulletValues.rateOfFire * 100;
+			rb.AddForce(transform.up * -bulletValues.force);
+			instanciatedBullet = Instantiate (equippedBullet, transform.position, transform.rotation) as GameObject;
 		}
-		else if (inputDevice.Action1.WasReleased && !isContinue) {
-			StopAllCoroutines();
-		}
+		counter--;
 	}
 }
