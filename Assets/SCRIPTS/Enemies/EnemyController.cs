81fda72a24d658abc0c38663ada1730bc78e54f9
noisegrrrl <emualynk@gmail.com>
2015-07-23 18:34:47 +0200

    Enemies maneuvers

--- Assets/SCRIPTS/Enemies/EnemyController.cs
+++ Assets/SCRIPTS/Enemies/EnemyController.cs
@@ -6,17 +6,48 @@ public class EnemyController : MonoBehaviour {
  	public Transform target;
 	public float speed;
 	public float rotationSpeed;
+	public float maneuverDistance;
+	bool avoiding;
+	int counter;
+	RaycastHit lastHit;
 
 	void Start() {
 		target = null;
+		avoiding = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (target != null) {
-			Quaternion rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position, transform.TransformDirection(-Vector3.forward)), rotationSpeed * Time.deltaTime);
-			transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
-			transform.position = transform.position + (transform.up * speed * Time.deltaTime);
+			RaycastHit hitInfo;
+			bool hit = Physics.Raycast(transform.position, transform.up, out hitInfo, maneuverDistance);
+			Quaternion rotation;
+			if (avoiding) {
+				rotation =  Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lastHit.transform.position + transform.right, transform.TransformDirection(-Vector3.forward)), rotationSpeed * Time.deltaTime);
+				transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+				transform.position = transform.position + (transform.up * speed * Time.deltaTime);
+				counter--;
+				if (counter <= 0) {
+					avoiding = false;
+				}
+			} else if (hit) {
+				if (hitInfo.collider.tag == "Enemy") {
+					rotation =  Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(hitInfo.transform.position + transform.right, transform.TransformDirection(-Vector3.forward)), rotationSpeed * Time.deltaTime);
+					transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+					transform.position = transform.position + (transform.up * speed * Time.deltaTime);
+					avoiding = true;
+					counter = 10;
+					lastHit = hitInfo;
+				} else { 
+					rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position, transform.TransformDirection(-Vector3.forward)), rotationSpeed * Time.deltaTime);
+					transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+					transform.position = transform.position + (transform.up * speed * Time.deltaTime);
+				}
+			} else { 
+				rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position, transform.TransformDirection(-Vector3.forward)), rotationSpeed * Time.deltaTime);
+				transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+				transform.position = transform.position + (transform.up * speed * Time.deltaTime);
+			}
 		}
 	}
 
