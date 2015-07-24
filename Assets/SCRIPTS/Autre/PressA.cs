f968c5d1d0a39766ecff2983ab618a519ee1453e
Paul Maurer <maurer.paul@ymail.com>
2015-07-25 00:37:18 +0200

    ecran de tips

--- Assets/SCRIPTS/Autre/PressA.cs
+++ Assets/SCRIPTS/Autre/PressA.cs
@@ -3,13 +3,13 @@ using System.Collections;
 using InControl;
 public class PressA : MonoBehaviour {
 
-
+	public string scene;
 	
 	// Update is called once per frame
 	void Update () {
 		var inputDevice = InputManager.ActiveDevice;
 		if (inputDevice.Action1.WasPressed) {
-			Application.LoadLevel("LevelSelection");
+			Application.LoadLevel(scene);
 		}
 	}
 }
