86300eb6746d92de570d94dcc327b5eb519b7967
Paul Maurer <maurer.paul@ymail.com>
2015-07-24 11:40:47 +0200

    pleins de trucs liés au flow

---
+++ Assets/SCRIPTS/Autre/Chrono.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chrono : MonoBehaviour {
+	
+	public Text chrono;
+	public Text yourTime;
+	private bool _isRunning;
+	private bool _wasRunningLastUpdate;
+	private float _elapsedSeconds;
+	private float _timeLastUpdate;
+	
+	
+	void Start() {
+		StartTimer ();
+		InvokeRepeating("updateTextMesh", 0, 0.2f);
+	}
+	
+	public void StartTimer() {
+		_isRunning = true;
+	}
+	
+	public void ResetTimer() {
+		_elapsedSeconds = 0;
+	}
+	
+	public void StopTimer() {
+		_isRunning = false;
+	}
+	
+	private void updateTextMesh() {
+		if (!_isRunning) {
+			_wasRunningLastUpdate = false;
+			return;
+		}
+		if (_wasRunningLastUpdate) {
+			var deltaTime = Time.time - _timeLastUpdate;
+			_elapsedSeconds += deltaTime;
+		}
+		
+		var timeSpan = TimeSpan.FromSeconds(_elapsedSeconds);
+		chrono.text = string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+		_timeLastUpdate = Time.time;
+		_wasRunningLastUpdate = true;
+	}
+}
---
+++ Assets/SCRIPTS/Autre/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class GameManager : MonoBehaviour {
+
+	GameObject theCore;
+	public GameObject thePlayer;
+	public GameObject spawner;
+	public GameObject startText;
+	public GameObject chrono;
+	public GameObject yourTime;
+	GameObject timer;
+	bool levelStarted;
+	DestroyByContact coreValues;
+	// Use this for initialization
+	void Start () {
+		levelStarted = false;
+		theCore = GameObject.FindGameObjectWithTag ("Core");
+		coreValues = theCore.GetComponent<DestroyByContact> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var inputDevice = InputManager.ActiveDevice;
+
+		if (inputDevice.Action1.WasPressed) {
+			startText.SetActive(false);
+			spawner.GetComponent<EnemyGenerator>().enabled = true;
+			thePlayer.GetComponent<Controller>().enabled = true;
+			chrono.SetActive(true);
+
+			levelStarted = true;
+		}
+		if (coreValues.life <= 0) {
+			spawner.GetComponent<EnemyGenerator>().enabled = false;
+			thePlayer.GetComponent<Controller>().enabled = false;
+			chrono.GetComponent<Chrono>().StopTimer();
+		}
+	}
+}
