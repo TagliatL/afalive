88afae774c795a152de9d8cec024076aab87f289
Paul Maurer <maurer.paul@ymail.com>
2015-07-24 17:06:21 +0200

    title screen

---
+++ Assets/SCRIPTS/Autre/PressA.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+public class PressA : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		var inputDevice = InputManager.ActiveDevice;
+		if (inputDevice.Action1.WasPressed) {
+			Application.LoadLevel("LevelSelection");
+		}
+	}
+}
