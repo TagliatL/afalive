cbd44bd7ad4c71a1878aa1f612e49b1514aa7a47
Paul Maurer <maurer.paul@ymail.com>
2015-07-25 02:00:24 +0200

    ajout de bails

---
+++ Assets/SCRIPTS/Autre/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	public Vector3 rotation;
+	public int speed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rotation * Time.deltaTime*speed);
+	}
+}
